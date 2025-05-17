2eedaa7936b15d4e5ef9b43a1aa5a80c673c491d
Kshitij-Halmare <horizon29042004@gmail.com>
2025-05-17 21:24:20 +0530

    Save work before pulling latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Route,Routes } from "react-router-dom"
 import Layout from "./Components/Layout"
 import Home from "./Pages/Home"
+import Register from "./Components/Register"
 function App() {
 
   return (
@@ -9,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>}></Route>
-
+        <Route path="register" element={<Register/>}></Route>
       </Route>
     </Routes>
     </>
