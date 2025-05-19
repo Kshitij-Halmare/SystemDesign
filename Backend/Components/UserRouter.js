56d751f80b8e3a423b5350c260a9478d4a868917
Kshitij-Halmare <horizon29042004@gmail.com>
2025-05-19 09:13:46 +0530

    User Route Schema setup

--- Backend/Components/UserRouter.js
+++ Backend/Components/UserRouter.js
@@ -1,4 +1,11 @@
 import User from "../Schemas/UserSchema.js"
 export async function Register(req,res) {
-    
+    const body=req.body;
+    if(!body){
+        return res.json({
+            message:"Send correct Data",
+            success:false,
+            error:true
+        })
+    }
 }
