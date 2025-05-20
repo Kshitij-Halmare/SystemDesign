58240b8357b2d63d4916c9c59a20f47b3b9020e2
Kshitij-Halmare <horizon29042004@gmail.com>
2025-05-20 08:43:34 +0530

    register User backend code done

--- Backend/Components/UserRouter.js
+++ Backend/Components/UserRouter.js
@@ -1,11 +1,45 @@
 import User from "../Schemas/UserSchema.js"
+import uploadImageCloudinary from "../Utils/uploadImage.js";
+import bcryptjs from "bcryptjs";
 export async function Register(req,res) {
-    const body=req.body;
-    if(!body){
-        return res.json({
-            message:"Send correct Data",
-            success:false,
-            error:true
+    const {name,email,password,occupation,dob}=req.body;
+    const file=req.file;
+    try{
+        const extinguisher=User.findOne({email:email});
+        if(extinguisher){
+             return res.status(400).json({
+                success: false,
+                error: true,
+                message: "User already exists, please log in.",
+            });
+        }
+        let imageurl=null;
+        if (file) {
+            const uploadResult = await uploadImageCloudinary(file.buffer); 
+            console.log(uploadResult);  
+            imageurl = uploadResult.secure_url;
+        }
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        const newUser=await User.create({
+            name,
+            email,
+            occupation,
+            dob,
+            password:hashedPassword,
+            imageurl         
         })
+         return res.status(200).json({
+            success: true,
+            error: false,
+            message: "User registered successfully. Please verify your email.",
+        });
+    }catch (error) {
+        console.error(error);  
+        return res.status(500).json({
+            success: false,
+            error: true,
+            message: `An error occurred: ${error.message}`,
+        });
     }
 }
